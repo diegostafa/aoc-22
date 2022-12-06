40932dbd5031f18e2e3cadd04d43ff7b9e06aab8
diegostafa <dstafa.dev@gmail.com>
2022-12-06 12:21:02 +0100

    day 6

---
+++ src/day-6/Program.fs
@@ -0,0 +1,39 @@
+﻿open System.IO
+
+module Part1 =
+
+    let rec scrollWindow (stream: char list) currPos windowSize =
+        match stream with
+        | [] -> currPos
+        | x :: xs ->
+            let buffer = (Seq.truncate windowSize stream) |> Seq.toList
+
+            if ((List.distinct buffer).Length = buffer.Length) then
+                currPos + windowSize
+            else
+                scrollWindow xs (currPos + 1) windowSize
+
+    let (main: unit) =
+        let charStream = File.ReadAllText("input") |> Seq.toList
+        let markerPos = scrollWindow charStream 0
+
+        printfn "Result: %i" (markerPos 4)
+
+module Part2 =
+
+    let rec scrollWindow (stream: char list) currPos windowSize =
+        match stream with
+        | [] -> currPos
+        | x :: xs ->
+            let buffer = (Seq.truncate windowSize stream) |> Seq.toList
+
+            if ((List.distinct buffer).Length = buffer.Length) then
+                currPos + windowSize
+            else
+                scrollWindow xs (currPos + 1) windowSize
+
+    let (main: unit) =
+        let charStream = File.ReadAllText("input") |> Seq.toList
+        let markerPos = scrollWindow charStream 0
+
+        printfn "Result: %i" (markerPos 14)
