37af1ec9510abbc8c8b1253678457fd2780bdd60
diegostafa <dstafa.dev@gmail.com>
2022-12-09 22:34:11 +0100

    prettify day 2

--- src/day-2/Program.fs
+++ src/day-2/Program.fs
@@ -2,53 +2,55 @@
 
 module Part1 =
     let toPair (line: string) =
-        let tokens = line.Split(" ") |> Array.toList
+        let tokens = line.Split(" ")
         (tokens[0], tokens[1])
 
-    let (moveValue: (string * string) -> int) =
-        fun (x, y) ->
-            match (x, y) with
-            | ("A", "X") -> 4
-            | ("A", "Y") -> 8
-            | ("A", "Z") -> 3
-            | ("B", "X") -> 1
-            | ("B", "Y") -> 5
-            | ("B", "Z") -> 9
-            | ("C", "X") -> 7
-            | ("C", "Y") -> 2
-            | ("C", "Z") -> 6
-            | (_, _) -> 0
+    let moveValue (x, y) =
+        match (x, y) with
+        | ("A", "X") -> 4
+        | ("A", "Y") -> 8
+        | ("A", "Z") -> 3
+        | ("B", "X") -> 1
+        | ("B", "Y") -> 5
+        | ("B", "Z") -> 9
+        | ("C", "X") -> 7
+        | ("C", "Y") -> 2
+        | ("C", "Z") -> 6
+        | (_, _) -> 0
 
 
     let (main: unit) =
-        let text = File.ReadAllText("input")
-        let lines: string list = text.Split("\n") |> Array.toList
-        let moves: (string * string) list = List.map toPair lines
-        let movesValue: (int list) = List.map moveValue moves
-        printfn "Total value is: %i" (List.sum movesValue)
+        printfn
+            "Result: %i"
+            (File.ReadAllText("input").Split("\n")
+             |> Array.toList
+             |> List.map toPair
+             |> List.map moveValue
+             |> List.sum)
 
 module Part2 =
     let toPair (line: string) =
-        let tokens = line.Split(" ") |> Array.toList
+        let tokens = line.Split(" ")
         (tokens[0], tokens[1])
 
-    let (moveValue: (string * string) -> int) =
-        fun (x, y) ->
-            match (x, y) with
-            | ("A", "X") -> 3
-            | ("A", "Y") -> 4
-            | ("A", "Z") -> 8
-            | ("B", "X") -> 1
-            | ("B", "Y") -> 5
-            | ("B", "Z") -> 9
-            | ("C", "X") -> 2
-            | ("C", "Y") -> 6
-            | ("C", "Z") -> 7
-            | (_, _) -> 0
+    let moveValue (x, y) =
+        match (x, y) with
+        | ("A", "X") -> 3
+        | ("A", "Y") -> 4
+        | ("A", "Z") -> 8
+        | ("B", "X") -> 1
+        | ("B", "Y") -> 5
+        | ("B", "Z") -> 9
+        | ("C", "X") -> 2
+        | ("C", "Y") -> 6
+        | ("C", "Z") -> 7
+        | (_, _) -> 0
 
     let (main: unit) =
-        let text = File.ReadAllText("input")
-        let lines: string list = text.Split("\n") |> Array.toList
-        let moves: (string * string) list = List.map toPair lines
-        let movesValue: (int list) = List.map moveValue moves
-        printfn "Total value is: %i" (List.sum movesValue)
+        printfn
+            "Total value is: %i"
+            (File.ReadAllText("input").Split("\n")
+             |> Array.toList
+             |> List.map toPair
+             |> List.map moveValue
+             |> List.sum)
