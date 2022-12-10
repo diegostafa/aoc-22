5d7958de1d6d05e8ebd9de3917187611b2d2b516
diegostafa <dstafa.dev@gmail.com>
2022-12-10 14:59:05 +0100

    day 10

--- src/day-10/Program.fs
+++ src/day-10/Program.fs
@@ -1,6 +1,66 @@
 ﻿open System.IO
 
 module Part1 =
-    let (main: unit) =
-        let input = File.ReadAllText("input").Split("\n") |> Seq.toList
-        printfn ""
+    type Cmd =
+        | Noop
+        | Addx of int
+
+    let parseCmd (cmd: string) =
+        match cmd.Split(" ") |> Seq.toList with
+        | _ :: [] -> [ Noop ]
+        | _ :: reg :: [] -> [ Noop; (Addx(int reg)) ]
+        | _ -> []
+
+    let evalProg (regCycles: int list, regVal) (cmd: Cmd) =
+        match cmd with
+        | Noop -> (regCycles @ [ regVal ]), regVal
+        | Addx reg -> (regCycles @ [ regVal ]), regVal + reg
+
+    let solve =
+        let regHist, _ =
+            File.ReadAllText("input").Split("\n")
+            |> Array.toList
+            |> List.collect parseCmd
+            |> List.fold evalProg ([ 1 ], 1)
+
+        printfn "Result: %A" (List.fold (fun acc i -> acc + regHist[i] * i) 0 [ 20..40 .. regHist.Length ])
+
+
+module Part2 =
+    type Cmd =
+        | Noop
+        | Addx of int
+
+    let parseCmd (cmd: string) =
+        match cmd.Split(" ") |> Seq.toList with
+        | _ :: [] -> [ Noop ]
+        | _ :: reg :: [] -> [ Noop; (Addx(int reg)) ]
+        | _ -> []
+
+    let evalProg (regCycles: int list, regVal) (cmd: Cmd) =
+        match cmd with
+        | Noop -> (regCycles @ [ regVal ]), regVal
+        | Addx reg -> (regCycles @ [ regVal ]), regVal + reg
+
+    let solve =
+        let regHist, _ =
+            File.ReadAllText("input").Split("\n")
+            |> Array.toList
+            |> List.collect parseCmd
+            |> List.fold evalProg ([ 1 ], 1)
+
+        let regs = List.chunkBySize 40 (List.tail regHist)
+
+        List.iter
+            (fun row ->
+                List.iter
+                    (fun col ->
+                        if col >= regs[row][col] - 1 && col <= regs[row][col] + 1 then
+                            printf "#"
+                        else
+                            printf " "
+
+                        if col = (regs[row].Length - 1) then
+                            printfn "")
+                    [ 0 .. (regs[row].Length - 1) ])
+            [ 0 .. (regs.Length - 1) ]
